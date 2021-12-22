db7f722c490ba28a6bacb0a57991655a4baa565b
tun-ji <oluwatomilola.adeniran@pau.edu.ng>
2021-12-22 22:42:19 +0100

    Templates & route definition commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Render('home.html')
+  getHome(): {} {
+    return this.appService.getHome();
+  }
+
+  @Get('about-us')
+  @Render('about-us.html')
+  getAboutUs(): {} {
+    return this.appService.getAboutUs();
+  }
+
+  @Get('hello2')
+  @Render('index.html')
+  getHello2(): {} {
+    console.log()
+    return this.appService.getHello2();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+//Let's make it a NestExpressApplication
+import {NestExpressApplication} from '@nestjs/platform-express';
+
+//We need join to synthesize the directory path which will contain templates
+import {join} from 'path';
+
+//We need nunjucks as render engine
+
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  
+  const staticAssets = join(__dirname, '..', 'static');
+  app.useStaticAssets(staticAssets);
+
+  const express = app.getHttpAdapter().getInstance();
+
+  const views = join(__dirname, '..', 'views');
+  /*Finally, configure nunjucks, setting views and express
+  declared above*/
+  nunjucks.configure(views, { express });
+  //start the application
   await app.listen(3000);
 }
+
 bootstrap();
